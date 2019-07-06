15a7833151e05d95398313a471136a24c64c0e92
Edward Duffy, Arch Laptop <edward70891@gmail.com>
2019-07-06 15:22:11 +0100

    Change to use pandigital from miscLibraries

--- python/38.py
+++ python/38.py
@@ -1,16 +1,14 @@
 #!/bin/env python3
-from collections import Counter
+from miscLibraries.python.pandigital import is_pandigital
 
 # Problem at:
 # https://projecteuler.net/problem=38
-
-PAN_DIGITS = Counter("123456789")
 
 valid_nums = list()
 for base in range(1, 33334):
     for mult_max in range(2, 10):
         pan_test = "".join([str(base * i) for i in range(1, mult_max)])
-        if Counter(pan_test) == PAN_DIGITS:
+        if is_pandigital(pan_test):
             valid_nums.append(pan_test)
 
 print(max(valid_nums))
